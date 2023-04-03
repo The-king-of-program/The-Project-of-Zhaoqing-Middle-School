910f3db6a2f90aa3540bf1c29ab94f2462bec499
The-king -of-program <98509708+The-king-of-program@users.noreply.github.com>
2023-04-03 16:47:26 +0800

    test01.py
    The test01.py is a test for the PC system.

---
+++ test01.py
@@ -0,0 +1,9 @@
+import math
+import sys
+
+a = input("Please be respect!Type your number here:")
+try:
+    print("What's the number "+a+"'s meaning?")
+except ValueError:
+    print("Please type the correct number!")
+   
